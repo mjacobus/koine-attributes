f68fbb72ab70fdea9cc91451ffe54654e0a89f2a
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-05-02 16:55:58 +0100

    Improved tests

--- spec/koine/attributes/adapter/integer_spec.rb
+++ spec/koine/attributes/adapter/integer_spec.rb
@@ -2,15 +2,20 @@ require 'spec_helper'
 
 RSpec.describe Koine::Attributes::Adapter::Integer do
   INVALID_ARGUMENTS = [
-    '1foo'
+    '1foo',
+    '1.1',
+    '-1.1'
   ].freeze
 
   COERCIONS = {
-    '1'      => 1,
-    '1.1'    => 1,
-    1.0      => 1,
-    1.1      => 1,
-    1.999999 => 1
+    '1'       => 1,
+    1.0       => 1,
+    1.1       => 1,
+    1.999999  => 1,
+    '-1'      => -1,
+    -1.0      => -1,
+    -1.1      => -1,
+    -1.999999 => -1
   }.freeze
 
   it 'extends Base' do
@@ -18,19 +23,24 @@ RSpec.describe Koine::Attributes::Adapter::Integer do
   end
 
   describe '#coerce' do
-    COERCIONS.each do |raw_value, expected|
-      let(:result) { subject.coerce(raw_value) }
+    specify 'returns :to_int when object implements it' do
+      value = double(to_int: 10)
 
+      expect(subject.coerce(value)).to eq(10)
+    end
+
+    COERCIONS.each do |raw_value, expected|
       it "coerces #{raw_value} coerce to #{expected}" do
+        result = subject.coerce(raw_value)
+
         expect(result).to be(expected)
+        expect(result).to be_frozen
       end
     end
 
     INVALID_ARGUMENTS.each do |value|
-      context "with invalid value value #{value}" do
-        it 'raises argument error' do
-          expect { subject.coerce(value) }.to raise_error(ArgumentError)
-        end
+      it "raises InvalidArgument error with #{value}" do
+        expect { subject.coerce(value) }.to raise_error(ArgumentError)
       end
     end
   end
