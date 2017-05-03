e44c098c2e18fb36793a1a01d128c5f3cd4f8c78
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-05-03 22:40:10 +0200

    Renamed file

---
+++ lib/koine/attributes/adapter/time.rb
@@ -0,0 +1,14 @@
+module Koine
+  module Attributes
+    module Adapter
+      class Time < Base
+        def coerce(value)
+          secure do
+            next value if value.is_a?(::Time)
+            ::Time.parse(value)
+          end
+        end
+      end
+    end
+  end
+end
